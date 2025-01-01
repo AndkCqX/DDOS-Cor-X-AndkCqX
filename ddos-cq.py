2e7356c680263150c12a4b1588017c0e4ff3299b
AndkCqX <3756372822@qq.com>
2025-01-01 03:35:07 -0500

    Update ddos-cq.py

--- ddos-cq.py
+++ ddos-cq.py
@@ -15,6 +15,14 @@ year = now.year
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 bytes = r._urandom(1490)
 
+print("""MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+M       YMM M       YMM MMP     YMM MP       MM
+M  mmmm   M M  mmmm   M M   mmm   M M  mmmmm  M
+M  MMMMM  M M  MMMMM  M M  MMMMM  M M        YM
+M  MMMMM  M M  MMMMM  M M  MMMMM  M MMMMMMM   M
+M  MMMM   M M  MMMM   M M   MMM   M M   MMM   M
+M        MM M        MM MMb     dMM Mb       dM
+MMMMMMMMMMM MMMMMMMMMMM MMMMMMMMMMM MMMMMMMMMMM""")
 print()
 print("github   : https://github.com/AndkCqX/DDOS-Cor-X-AndkCqX")
 print("Telegram : @AndkCq")
