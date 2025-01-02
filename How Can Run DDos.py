7f2e73acb24fef6ce0c3d46364b702ad02c8215a
AndkCqX <3756372822@qq.com>
2025-01-02 08:37:51 -0500

    Update and rename can go to.txt to How Can Run DDos.py

---
+++ How Can Run DDos.py
@@ -0,0 +1,16 @@
+# can go to
+
+print("pkg install python")
+
+print("pkg install git -y")
+
+print("""git clone 
+https://github.com/AndkCqX/DDOS-Cor-X-AndkCqX""")
+
+print("cd DDOS-Cor-X-AndkCqX"
+
+print("""# kali
+python3 ddos-drak_matter.py""")
+
+print("""# termux 
+python3 ddos-drak_matter.py""")
