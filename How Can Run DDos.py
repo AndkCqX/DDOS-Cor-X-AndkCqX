ffc7b9121c95836772c61a1d8e6d704ea494637c
AndkCqX <3756372822@qq.com>
2025-01-02 08:38:25 -0500

    Update How Can Run DDos.py

--- How Can Run DDos.py
+++ How Can Run DDos.py
@@ -1,16 +1,16 @@
 # can go to
 
 print("pkg install python")
-
+t.sleep(0.05)
 print("pkg install git -y")
-
+t.sleep(0.05)
 print("""git clone 
 https://github.com/AndkCqX/DDOS-Cor-X-AndkCqX""")
-
+t.sleep(0.05)
 print("cd DDOS-Cor-X-AndkCqX"
-
+t.sleep(0.05)
 print("""# kali
 python3 ddos-drak_matter.py""")
-
+t.sleep(0.05)
 print("""# termux 
 python3 ddos-drak_matter.py""")
